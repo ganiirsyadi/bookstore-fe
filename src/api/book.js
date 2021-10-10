103f658ed6d569e854169e82d375a0ed9afd03e6
ganiilhamirsyadi@gmail.com <ganiilhamirsyadi@gmail.com>
2021-10-11 01:25:52 +0700

    fix addBook issue

--- src/api/book.js
+++ src/api/book.js
@@ -3,6 +3,5 @@ import axios from "axios";
 export const getBooks = () =>
   axios.get("https://5de759a9b1ad690014a4e21e.mockapi.io/api/v1/books");
 
-export const addBook = (data) => {
+export const addBook = (data) =>
   axios.post("https://5de759a9b1ad690014a4e21e.mockapi.io/api/v1/books", data);
-};
