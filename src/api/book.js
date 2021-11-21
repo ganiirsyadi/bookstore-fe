cebbe6a354aa0f2aaccc0683c096637a4f8c1f3b
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2021-11-21 11:13:39 +0700

    add .env config for API_BASE_URL

--- src/api/book.js
+++ src/api/book.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const url = process.env.REACT_APP_API_URL
+
 export const getBooks = () =>
-  axios.get("https://5de759a9b1ad690014a4e21e.mockapi.io/api/v1/books");
+  axios.get(url);
 
 export const addBook = (data) =>
-  axios.post("https://5de759a9b1ad690014a4e21e.mockapi.io/api/v1/books", data);
+  axios.post(url, data);
