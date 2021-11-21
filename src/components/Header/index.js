e91f0a0d71313c1b8331e17fe9faa24542fecf33
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2021-11-21 16:28:43 +0700

    change title

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <header data-testid="header" className={styles.header}>
-      BOOK STORE
+      RATE YOUR BOOK
     </header>
   );
 };
